4fa516220b6cc238d1696bd52e9ab4c9972b8ce6
CodewithRubs <159712173+codewithrubz@users.noreply.github.com>
2024-03-16 02:14:02 +0500

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
-let quote: string= "A person who never made amistake never tried anything new";
-let author: string= "Albert Einstein";
-console.log(` ${author} once said, "${quote}`);
+let quote: string= "A person who never made a mistake never tried anything new";
+let famous_person="Albert Einstein";
+let message=`${famous_person} once said, ${quote}`;
+console.log(message);
