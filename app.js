7b8e0c1a8dd81d2477a0a389f99ab32d150c4932
CodewithRubs <159712173+codewithrubz@users.noreply.github.com>
2024-03-16 02:12:53 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-/*Animals: Think of at least three different animals that have a common characteristic.
- Store the names of these animals in a list, and then use a for loop to print out the name of each animal.
-  • Modify your program to print a statement about each animal, such as A dog would make a great pet.
-  • Add a line at the end of your program stating what these animals have in common. You could print a sentence such as Any of these animals would make a great pet! */
-var Animals = ["Cat", "Goat", "Rabbit"];
-for (var pets = 0; pets < Animals.length; pets++) {
-    console.log("A ".concat(Animals[pets], " is a loyal pet"));
-}
-console.log("All these animals become bestfriends and loyal with human, if owners treat them with kindness.");
+var quote = "A person who never made amistake never tried anything new";
+var author = "Albert Einstein";
+console.log(" ".concat(author, " once said, \"").concat(quote));
